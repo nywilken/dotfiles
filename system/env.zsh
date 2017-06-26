15ad8a78970c7e9d81eca2362ddab3b0e57ef01d
nywilken <dev@wilkenrivera.com>
2017-06-26 08:16:51 -0400

    Move Go envs to localrc

--- system/env.zsh
+++ system/env.zsh
@@ -3,7 +3,3 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
   export EDITOR='subl'
 fi
-
-if [[ "$GOPATH" == "" ]] ; then
-  export GOPATH=$HOME/Development/golang
-fi
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/sbin/:/usr/local/sbin:$HOME/.sfs:$HOME/bin:$ZSH/bin:/usr/local/go/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/sbin/:/usr/local/sbin:$HOME/.sfs:$HOME/bin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
