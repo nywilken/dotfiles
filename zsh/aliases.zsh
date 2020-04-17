3d04630580d0621a72c7d9f045eb49b58a1a3802
Wilken Rivera <dev@wilkenrivera.com>
2020-04-17 07:38:18 -0400

    Add some functions to managed todo list in terminal

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,3 +12,34 @@ alias grep='grep  --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}'
 alias ack='ack  --ignore-dir={.git,vendor,.vagrant}'
 alias tmux='tmux -2'
 
+new-thing() {
+  pushd ~/Documents/gsd
+  hub issue create -m $1 -l $2
+  popd
+}
+complete-thing() {
+  pushd ~/Documents/gsd
+  hub issue update $1 -l done -s closed
+  popd
+}
+read-thing() {
+  pushd ~/Documents/gsd
+  hub issue show $1 | less
+  popd
+}
+thing-report() {
+  DATEFILTER=$(date --date="-1 day" -I)
+  if [ ! -z $1 ];
+  then
+    DATEFILTER=$(date --date="-1 month" -I)
+  fi
+
+  pushd ~/Documents/gsd
+  echo "Things Completed"
+  hub issue -l done -s closed -d $DATEFILTER -f "(%I) * %t%n"
+  echo "Things Up Next"
+  hub issue -l today, next -f "(%I) * %t%n"
+  popd
+
+}
+
